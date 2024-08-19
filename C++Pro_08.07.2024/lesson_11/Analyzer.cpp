06c1601f22aa0dd725dcbfad950199bd017d9073
Respecter1 <168243002+Respecter1@users.noreply.github.com>
2024-08-19 23:32:28 +0300

    Update Analyzer.cpp

--- C++Pro_08.07.2024/lesson_11/Analyzer.cpp
+++ C++Pro_08.07.2024/lesson_11/Analyzer.cpp
@@ -8,49 +8,36 @@ Analyzer::Analyzer(QObject* parent)
 {
 }
 
-void Analyzer::analyzeData(const SensorMetric& aSensorMetric)
-{
+void Analyzer::analyzeData(const SensorMetric& aSensorMetric) {
     sensorData[aSensorMetric.name].push_back(aSensorMetric.value);
     allValues.push_back(aSensorMetric.value);
 
     if (allValues.size() > Config::MAX_RECORDS) {
-        allValues.remove(0);
+        allValues.removeAt(0);
     }
     if (sensorData[aSensorMetric.name].size() > Config::MAX_RECORDS) {
-        sensorData[aSensorMetric.name].remove(0);
+        sensorData[aSensorMetric.name].removeAt(0);
     }
 }
 
-void Analyzer::printStats(const QString& title, const QVector<int>& values) const
-{
-    if (values.isEmpty()) {
-        qDebug() << title.toStdString() << ": No data available";
+void Analyzer::reportPrint() const {
+    if (allValues.isEmpty()) {
+        qDebug() << "No data available for statistics.";
         return;
     }
 
-    int maxVal = *std::max_element(values.begin(), values.end());
-    int minVal = *std::min_element(values.begin(), values.end());
-    double average = std::accumulate(values.begin(), values.end(), 0.0) / values.size();
+    int maxVal = *std::max_element(allValues.begin(), allValues.end());
+    int minVal = *std::min_element(allValues.begin(), allValues.end());
+    double average = std::accumulate(allValues.begin(), allValues.end(), 0.0) / allValues.size();
 
-    QVector<int> sortedValues = values;
+    QList<int> sortedValues = allValues;
     std::sort(sortedValues.begin(), sortedValues.end());
     int median = sortedValues[sortedValues.size() / 2];
 
-    qDebug() << title.toStdString();
-    qDebug() << "  Max: " << maxVal;
-    qDebug() << "  Min: " << minVal;
-    qDebug() << "  Average: " << average;
-    qDebug() << "  Median: " << median;
+    qDebug() << "Overall Stats";
+    qDebug() << "  Max:" << maxVal;
+    qDebug() << "  Min:" << minVal;
+    qDebug() << "  Average:" << average;
+    qDebug() << "  Median:" << median;
 }
 
-void Analyzer::reportPrint() const
-{
-    printStats("Overall Stats", allValues);
-}
-
-void Analyzer::reset()
-{
-    // Очищуємо всі зібрані дані
-    sensorData.clear();
-    allValues.clear();
-}
