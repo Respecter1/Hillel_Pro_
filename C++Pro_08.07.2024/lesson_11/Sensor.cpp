3cc0c843a5a5816f0562c43ba1fdc26515210d80
Respecter1 <168243002+Respecter1@users.noreply.github.com>
2024-08-19 23:36:58 +0300

    Update Sensor.cpp

--- C++Pro_08.07.2024/lesson_11/Sensor.cpp
+++ C++Pro_08.07.2024/lesson_11/Sensor.cpp
@@ -1,13 +1,23 @@
 #include "Sensor.h"
 
 Sensor::Sensor(const QString& sensorName, QObject* parent)
-    : QObject(parent), name(sensorName)
-{ }
+    : QObject(parent), name(sensorName), currentValue(0)
+{
+}
 
 void Sensor::newData(int value)
 {
+    currentValue = value;
     SensorMetric metric;
     metric.name = name;
-    metric.value = value;
+    metric.value = currentValue;
     emit dataReady(metric);
+}
+
+SensorMetric Sensor::getMetric() const
+{
+    SensorMetric metric;
+    metric.name = name;
+    metric.value = currentValue;
+    return metric;
 }
